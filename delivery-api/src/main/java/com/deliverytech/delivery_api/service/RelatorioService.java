e51bebbc4b30f192460b5b6ab7ce32517693c601
willian-brandao <willianramos.wrk@gmail.com>
2026-01-25 19:01:24 -0300

    Criada a classe relatorio service para definir as regras de negocio de relatorio

---
+++ delivery-api/src/main/java/com/deliverytech/delivery_api/service/RelatorioService.java
@@ -0,0 +1,22 @@
+package com.deliverytech.delivery_api.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.deliverytech.delivery_api.repository.PedidoRepository;
+import com.deliverytech.delivery_api.dto.TotalDeVendasPorRestauranteDTO;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class RelatorioService {
+    
+    private final PedidoRepository pedidoRepository;
+
+    List<TotalDeVendasPorRestauranteDTO> totalDeVendasPorRestaurante(){
+        return pedidoRepository.totalDeVendasPorRestaurante();
+    }
+
+}
