e7193c23af5c54122a0423c035204e255161ce67
willian-brandao <willianramos.wrk@gmail.com>
2026-02-03 17:45:25 -0300

    Criado as classes de exceção

---
+++ delivery-api/src/main/java/com/deliverytech/delivery_api/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    
+    @ExceptionHandler(EntityNotFoundException.class)
+    public  ResponseEntity<String> handleNotFound(EntityNotFoundException ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<String> handleBusiness(BusinessException ex){
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+}
