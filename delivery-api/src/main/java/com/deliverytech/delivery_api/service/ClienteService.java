6e4cb686966ecea4d9f132c2c081fab4035d8ef9
willian-brandao <willianramos.wrk@gmail.com>
2026-02-03 18:25:47 -0300

    modificado as classes do service de restaurante e cliente para receber o mapper

--- delivery-api/src/main/java/com/deliverytech/delivery_api/service/ClienteService.java
+++ delivery-api/src/main/java/com/deliverytech/delivery_api/service/ClienteService.java
@@ -5,8 +5,12 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import com.deliverytech.delivery_api.dto.responses.ClienteResponseDTO;
+import com.deliverytech.delivery_api.exceptions.BusinessException;
+import com.deliverytech.delivery_api.exceptions.EntityNotFoundException;
 import com.deliverytech.delivery_api.model.Cliente;
 import com.deliverytech.delivery_api.repository.ClienteRepository;
 
@@ -18,22 +22,83 @@ import jakarta.transaction.Transactional;
 public class ClienteService {
     
     private ClienteRepository repository;
+    private ModelMapper mapper;
 
-    public ClienteService(ClienteRepository repository){
+    public ClienteService(ClienteRepository repository, ModelMapper mapper){
         this.repository = repository;
+        this.mapper = mapper;
     }
 
     //método de cadastro 
-    public Cliente cadastrar(Cliente cliente){
+    public ClienteResponseDTO cadastrar(Cliente dto){
         
-        if( repository.existsByEmail( cliente.getEmail() )){
-            throw new IllegalArgumentException("E-mail já cadastrado");
+        if( repository.existsByEmail( dto.getEmail() )){
+            throw new BusinessException("E-mail já cadastrado");
         }
+
+        Cliente cliente = mapper.map(dto, Cliente.class);
+
         cliente.setAtivo(true);
         cliente.setDataCadastro(LocalDateTime.now());
-        return repository.save(cliente);
+        
+        Cliente salvo = repository.save(cliente);
+        return mapper.map(salvo, ClienteResponseDTO.class);
     }
+
+
     //método de listar dados
+    public List<ClienteResponseDTO> listarAtivos(){
+        return repository.findByAtivoTrue()
+        .stream()
+        .map(clientes -> mapper.map(clientes, ClienteResponseDTO.class))
+        .toList();
+    }
+
+
+    //método de encontrar nomes
+    public List<ClienteResponseDTO> buscarPorNome(String nome){
+        return repository.findByNomeContainingIgnoreCase(nome)
+        .stream()
+        .map(cliente -> mapper.map(cliente, ClienteResponseDTO.class))
+        .toList();
+            
+    }
+
+    public ClienteResponseDTO buscarPorId(Long id){
+        
+        Cliente cliente = repository.findById(id)
+        .orElseThrow(()-> new EntityNotFoundException("Cliente não encontrado"));
+        return mapper.map(cliente, ClienteResponseDTO.class);
+    }
+/* 
+    public Cliente atualizar(Long id, Cliente dadosAtualizados){
+        
+        //busca pelo cliente pelo id
+        Cliente cliente = buscarPorId(id);
+
+        cliente.setNome(dadosAtualizados.getNome());
+        cliente.setEmail(dadosAtualizados.getEmail());
+        cliente.setTelefone(dadosAtualizados.getTelefone());
+        cliente.setEndereco(dadosAtualizados.getEndereco());
+
+        return repository.save(cliente);
+
+    }
+*/
+
+    @Transactional
+    public ClienteResponseDTO toggleAtivo(long id){
+        Cliente cliente = repository.findById(id)
+            .orElseThrow(()-> new EntityNotFoundException("Cliente não encontrado"));
+
+        cliente.setAtivo(!cliente.isAtivo());
+        return mapper.map(cliente, ClienteResponseDTO.class);
+    }
+}
+
+
+/*
+//método de listar dadosSS
     public List<Cliente> listarAtivos(){
         return repository.findByAtivoTrue();
     }
@@ -67,4 +132,4 @@ public class ClienteService {
         cliente.setAtivo(false);
         repository.save(cliente);
     }
-}
+*/
--- delivery-api/src/main/java/com/deliverytech/delivery_api/service/RestauranteService.java
+++ delivery-api/src/main/java/com/deliverytech/delivery_api/service/RestauranteService.java
@@ -43,3 +43,39 @@ public class RestauranteService {
     }
 }
 
+
+/***
+ public class RestauranteService {
+    
+    private final RestauranteRepository repository;
+
+    public RestauranteService( RestauranteRepository repository){
+
+        this.repository = repository;
+    }
+
+    // cadastro do restaurante
+    public Restaurante cadastrar(Restaurante dados){
+        dados.setAtivo(true);
+        return repository.save(dados);
+    }   
+
+    public List<Restaurante> listarRestauranteAtivos(){
+        return repository.findByAtivoTrue();
+    }
+
+    public List<Restaurante> buscarPorRestaurantesPorCategoria( String categoria){
+        return repository.findByCategoriaAndAtivoTrue(categoria);
+    }
+
+    public Restaurante buscarPorId( Long  id){
+        return repository.findById( id )
+        .orElseThrow(()-> new IllegalArgumentException("Restaurante não ecncontrado"));
+    }
+
+    public void desativar(Long id){
+        Restaurante restaurante = buscarPorId(id);
+        restaurante.setAtivo(false);
+        repository.save(restaurante);
+    }
+ */
